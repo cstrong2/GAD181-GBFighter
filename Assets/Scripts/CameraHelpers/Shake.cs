a568ae97fd5f9010a18be35381e0dd7180564a4e
solidfry <ellioteweb@gmail.com>
2022-08-19 13:26:06 +1000

    Added screen punch to fight

---
+++ Assets/Scripts/CameraHelpers/Shake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DG.Tweening;
+using Events;
+using UnityEngine;
+
+namespace CameraHelpers
+{
+    public class Shake : MonoBehaviour
+    {
+        [SerializeField] private Camera cam;
+
+        private void OnEnable()
+        {
+            GameEvents.OnAttackLandedEvent += DoShake;
+        }
+        
+        private void OnDisable()
+        {
+            GameEvents.OnAttackLandedEvent -= DoShake;
+        }
+
+        private void DoShake()
+        {
+            cam.DOShakePosition(.02f, .2f, 1, 45);
+        }
+    }
+}
